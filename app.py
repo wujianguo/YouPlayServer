7fddc00fea7eda45fb402f3612b40ce9d6cbbc28
wujianguo <github@wujianguo.org>
2015-11-12 21:52:01 +0800

    api

--- app.py
+++ app.py
@@ -7,24 +7,56 @@ import tornado.web
 import tornado.wsgi
 
 class MainHandler(tornado.web.RequestHandler):
+
     def get(self):
         self.write("Hello, world")
 
+
+class APIHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("Hello, world")
+
+
+class SearchHandler(tornado.web.RequestHandler):
+    '''Search http://www.soku.com
+
+    '''
+    def get(self):
+        self.write("Hello, world")
+
+
+class TeleplayListHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("Hello, world")
+
+
 class VideosHandler(tornado.web.RequestHandler):
-    # https://youplay.avosapps.com/api/v1/videos/{base64 url}
-    # example: https://youplay.avosapps.com/api/v1/videos/aHR0cDovL3R2LnNvaHUuY29tLzIwMTUxMTAzL240MjUxNTgwMDQuc2h0bWw=
+    '''Extract videos api
+
+    https://youplay.avosapps.com/api/v1/videos/{base64 url}
+    example:
+        https://youplay.avosapps.com/api/v1/videos/aHR0cDovL3R2LnNvaHUuY29tLzIwMTUxMTAzL240MjUxNTgwMDQuc2h0bWw=
+    '''
 
     def get(self, url):
+        url = url.split('/')[0]
         self.set_header('Content-Type', 'application/json')
         self.write(json.dumps({"k1": url, "k2": [{"k": 1}, {"k2": "s"}]}))
+
 
 settings = {"debug": True}
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/api/v1", APIHandler),
     (r"/api/v1/videos/(.*)", VideosHandler),
+    ("r/api/v1/search", SearchHandler),
+    ("r/api/v1/channel/teleplaylist", TeleplayListHandler)
 ], **settings)
 
 app = tornado.wsgi.WSGIAdapter(application)
+
 
 def main():
     application.listen(8888)
