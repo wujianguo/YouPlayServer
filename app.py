715c0b6a6a482c8990dea88080e0d8ddf7a127c9
wujianguo <lsjustin89@gmail.com>
2016-01-07 13:11:00 +0800

    soku

--- app.py
+++ app.py
@@ -9,7 +9,95 @@ import tornado.wsgi
 import HTMLParser
 import requests
 import json
+import base64
+import re
+import time
+from random import random
+from urlparse import urlparse
 
+fake_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'UTF-8,*;q=0.5',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20100101 Firefox/13.0'
+}
+
+
+# DEPRECATED in favor of match1()
+def r1(pattern, text):
+    m = re.search(pattern, text)
+    if m:
+        return m.group(1)
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+get_decoded_html = get_html
+
+class SohuExtractor:
+
+    def __init__(self, url):
+        self.url = url
+
+    def real_url(self, host, vid, tvid, new, clipURL, ck):
+        url = 'http://'+host+'/?prot=9&prod=flash&pt=1&file='+clipURL+'&new='+new +'&key='+ ck+'&vid='+str(vid)+'&uid='+str(int(time.time()*1000))+'&t='+str(random())+'&rb=1'
+        return json.loads(get_html(url))['url']
+
+    def extract(self, meta, quality="highVid"):
+        if re.match(r'http://share.vrs.sohu.com', self.url):
+            vid = r1('id=(\d+)', url)
+        else:
+            html = get_html(self.url)
+            vid = r1(r'\Wvid\s*[\:=]\s*[\'"]?(\d+)[\'"]?', html)
+        assert vid
+
+        infos = []
+        if re.match(r'http://tv.sohu.com/', self.url):
+            info = json.loads(get_decoded_html('http://hot.vrs.sohu.com/vrs_flash.action?vid=%s' % vid))
+            for qtyp in ["oriVid","superVid","highVid" ,"norVid","relativeId"]:
+                hqvid = info['data'][qtyp]
+                if hqvid != 0 and hqvid != vid :
+                    info = json.loads(get_decoded_html('http://hot.vrs.sohu.com/vrs_flash.action?vid=%s' % hqvid))
+
+                    host = info['allot']
+                    prot = info['prot']
+                    tvid = info['tvid']
+                    urls = []
+                    data = info['data']
+                    title = data['tvName']
+                    size = sum(data['clipsBytes'])
+                    assert len(data['clipsURL']) == len(data['clipsBytes']) == len(data['su'])
+                    if meta:
+                        item = {
+                            "quality": qtyp,
+                            "totalDuration": data["totalDuration"],
+                            "totalBytes": data["totalBytes"],
+                            "clipsBytes": data["clipsBytes"],
+                            "clipsDuration": data["clipsDuration"]
+                            }
+                        infos.append(item)
+                        continue
+
+                    if quality == qtyp:
+                        for new,clip,ck, in zip(data['su'], data['clipsURL'], data['ck']):
+                            clipURL = urlparse(clip).path
+                            urls.append(self.real_url(host,hqvid,tvid,new,clipURL,ck))
+
+                        item = {
+                            "urls": urls,
+                            "quality": qtyp,
+                            "totalDuration": data["totalDuration"],
+                            "totalBytes": data["totalBytes"],
+                            "clipsBytes": data["clipsBytes"],
+                            "clipsDuration": data["clipsDuration"]
+                            }
+                        return item
+
+
+        return infos
 
 class DetailInfoHtmlParser(HTMLParser.HTMLParser):
 
@@ -308,10 +396,12 @@ class VideosHandler(tornado.web.RequestHandler):
     '''
 
     def get(self, url):
-        url = url.split('/')[0]
+        origin_url = base64.b64decode(url.split('/')[0])
+        meta = self.get_query_argument("meta", 0)
+        quality = self.get_query_argument("quality", None)
         self.set_header('Content-Type', 'application/json')
-        self.write(json.dumps({"k1": url, "k2": [{"k": 1}, {"k2": "s"}]}))
-
+        sohu = SohuExtractor(origin_url)
+        self.write(json.dumps({"err":0, "msg":"", "data":sohu.extract(meta, quality)}))
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "public"),
